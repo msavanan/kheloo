a25f41e1becf6a9ad483ebdc23cb740a1224791c
msrvna <saravanan.rnd@gmail.com>
2024-01-16 00:57:51 +0530

    jackpot number

--- lib/utlis/jackpot.dart
+++ lib/utlis/jackpot.dart
@@ -1,15 +1,52 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:kheloo/const/img.dart';
 
-class JackPotNumber extends StatelessWidget {
+class JackPotNumber extends StatefulWidget {
   const JackPotNumber({super.key});
 
   @override
+  State<JackPotNumber> createState() => _JackPotNumberState();
+}
+
+class _JackPotNumberState extends State<JackPotNumber> {
+  int jackpot = 11105200;
+  late Timer timer;
+
+  @override
+  void initState() {
+    timer = Timer.periodic(const Duration(seconds: 1), (count) {
+      setState(() {
+        jackpot += 1;
+      });
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      Img.jackpotBanner,
-      width: 320,
-      height: 145,
+    return Column(
+      children: [
+        Container(height: 50),
+        Stack(
+          children: [
+            Image.asset(
+              Img.jackpotBanner,
+              width: 320,
+              height: 145,
+            ),
+            Positioned(
+                top: 55,
+                left: 70,
+                child: Text(
+                  jackpot.toString().split('').join(' '),
+                  style: const TextStyle(fontSize: 38, color: Colors.red),
+                ))
+          ],
+        ),
+        Container(height: 20),
+      ],
     );
   }
 }
